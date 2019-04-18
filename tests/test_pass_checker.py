50e46ffa6b1970a5eabc6bb283bee5f96559e8e3
chrisribia <31062468+chrisribia@users.noreply.github.com>
2019-04-18 11:37:35 +0300

    update pass validator

--- tests/test_pass_checker.py
+++ tests/test_pass_checker.py
@@ -10,32 +10,32 @@ class TestValidPassword(unittest.TestCase):
         self.assertEqual(
             validate_password(), "INVALID: Password should have minumum of 6 characters and maximum of 12 characters")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='Danieldanny2547@')
+    @patch('pass_checker.pass_checker.get_password', return_value='Chris2547@')
     def test_password_maximum_length(self, input):
         self.assertEqual(
             validate_password(), "INVALID: Password should have minumum of 6 characters and maximum of 12 characters")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='DAN254@')
+    @patch('pass_checker.pass_checker.get_password', return_value='CHRIS254@')
     def test_password_lowercase_character(self, input):
         self.assertEqual(
             validate_password(), "INVALID: Password should contain at least a lowercase character")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='DANiel@')
+    @patch('pass_checker.pass_checker.get_password', return_value='CHRISiel@')
     def test_password_should_contain_number(self, input):
         self.assertEqual(
             validate_password(), "INVALID: Password should contain at least a number")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='daniel1@')
+    @patch('pass_checker.pass_checker.get_password', return_value='chris1@')
     def test_password_should_contain_uppercase_character(self, input):
         self.assertEqual(
             validate_password(), "INVALID: Password should contain at least a capital character")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='Daniel112')
+    @patch('pass_checker.pass_checker.get_password', return_value='Chrisl112')
     def test_password_should_contain_special_character(self, input):
         self.assertEqual(
             validate_password(), "INVALID: Password should contain at least a special character")
 
-    @patch('pass_checker.pass_checker.get_password', return_value='Daniel254@#')
+    @patch('pass_checker.pass_checker.get_password', return_value='Chrs254@#')
     def test_valid_password(self, input):
         self.assertEqual(
             validate_password(), "Your password is VALID")
